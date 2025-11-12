b1e30c6631756093bfa1ed91f96ce56c6e3ac0f6
Kasper Declercq <96490838+Kasp100@users.noreply.github.com>
2025-11-12 11:03:38 +0100

    Read API key and print JSON response

--- Challenge1.cs
+++ Challenge1.cs
@@ -1,16 +1,18 @@
-﻿using System.Net.Http.Headers;
-
-namespace Hack_The_Future;
+﻿namespace Hack_The_Future;
 
 public class Challenge1
 {
     public static void Main()
     {
+        string apiKey = File.ReadAllText(@"key.txt");
+        
         HttpClient client = new HttpClient()
         {
             BaseAddress = new Uri("https://exs-htf-2025.azurewebsites.net")
         };
         
-        
+        client.DefaultRequestHeaders.Add("Authorization", "team " + apiKey);
+        string res = client.GetAsync("/api/challenges/signal").Result.Content.ReadAsStringAsync().Result;
+        Console.WriteLine(res);
     }
 }
